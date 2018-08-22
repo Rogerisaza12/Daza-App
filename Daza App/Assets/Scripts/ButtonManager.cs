e410076a94dc53c3caef439921e6596ab03eaa7f
Alejandro Isaza O <isaza10121@gmail.com>
2018-08-22 14:02:54 -0500

    First MVP progress

---
+++ Daza App/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+    public GameObject camera;
+
+
+    public void Volver()
+    {
+        camera.transform.position = new Vector3(0f, 0f, -10f);
+    }
+    public void Propuestas()
+    {
+        camera.transform.position = new Vector3(12f, 5f, -10f);
+    }
+    public void Familia()
+    {
+        camera.transform.position = new Vector3(-12f, 5f, -10f);
+    }
+    public void Estudio()
+    {
+        camera.transform.position = new Vector3(12f, -5f, -10f);
+    }
+    public void Experiencia()
+    {
+        camera.transform.position = new Vector3(-12f, -5f, -10f);
+    }
+}
---
+++ Daza App/Assets/Scripts/CameraMovemen.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovemen : MonoBehaviour
+{
+
+    private Vector3 dragOrigin; 
+    private Vector3 clickOrigin = Vector3.zero; 
+    private Vector3 basePos = Vector3.zero; 
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if (clickOrigin == Vector3.zero)
+            {
+                clickOrigin = Input.mousePosition;
+                basePos = transform.position;
+            }
+            dragOrigin = Input.mousePosition;
+        }
+
+        if (!Input.GetMouseButton(0))
+        {
+            clickOrigin = Vector3.zero;
+            return;
+        }
+
+        transform.position = new Vector3(basePos.x + ((clickOrigin.x - dragOrigin.x) * .04f), basePos.y + ((clickOrigin.y - dragOrigin.y) * .04f), -10);
+    }
+}
