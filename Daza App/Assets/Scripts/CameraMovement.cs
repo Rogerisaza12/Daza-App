11558503833d07ac07e8402e764f1e34b9b8be76
Alejandro Isaza O <isaza10121@gmail.com>
2018-08-22 14:18:38 -0500

    Camera Boundaries Set

--- Daza App/Assets/Scripts/CameraMovement.cs
+++ Daza App/Assets/Scripts/CameraMovement.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraMovemen : MonoBehaviour
+public class CameraMovement : MonoBehaviour
 {
 
     private Vector3 dragOrigin; 
     private Vector3 clickOrigin = Vector3.zero; 
-    private Vector3 basePos = Vector3.zero; 
-
+    private Vector3 basePos = Vector3.zero;
+    public bool bounds;
+    public Vector3 minCameraPos;
+    public Vector3 maxCameraPos;
     void Update()
     {
         if (Input.GetMouseButton(0))
@@ -28,5 +30,15 @@ public class CameraMovemen : MonoBehaviour
         }
 
         transform.position = new Vector3(basePos.x + ((clickOrigin.x - dragOrigin.x) * .04f), basePos.y + ((clickOrigin.y - dragOrigin.y) * .04f), -10);
+
+        if (bounds)
+        {
+            transform.position = new Vector3
+                (
+                    Mathf.Clamp(transform.position.x, minCameraPos.x, maxCameraPos.x),
+                    Mathf.Clamp(transform.position.y, minCameraPos.y, maxCameraPos.y),
+                    Mathf.Clamp(transform.position.z, minCameraPos.z, maxCameraPos.z)
+                );
+        }
     }
 }
